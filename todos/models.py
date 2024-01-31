61100fbae8d3fd15e11b94fc6bb63f67ef0296bc
Jordyn Kuhn <jkuhn2472@mail.kvcc.edu>
2024-01-31 17:33:20 -0500

    Add Date/Time for todos. Update display for todo models in home.html.

--- todos/models.py
+++ todos/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 class Todo(models.Model):
     text = models.TextField()
+    complete_by = models.DateTimeField(null=True)
 
     def __str__(self):
         """String method"""
